071f940639d3aac942d052a45ac73cf1e3a4413e
tjcombs852 <123331074+tjcombs852@users.noreply.github.com>
2023-01-22 14:48:01 -0700

    Update PetApp.java
    Added colors to the pets added via the petApp.

--- PetApp.java
+++ PetApp.java
@@ -17,15 +17,15 @@ public class PetApp {
 		st.put((int)(10+Math.random()*20.0), new Pet(8, "Buddy", "dog"));
 		
 		Pet[] pets = new Pet[] {
-				new Pet (1, "Bailey", "cat"),
-				new Pet (2, "Milo", "mouse"),
-				new Pet(3,"Otis","mouse"),
-                new Pet(4,"Bella","cat"),
-                new Pet(5,"Derik","parrot"),
-                new Pet(6,"Maggie","dog"),
-                new Pet(7,"Ranger","dog"),
-                new Pet(8,"Danny","ferret"),
-                new Pet(10,"Rosko","dog")
+				new Pet (1, "Bailey", "cat", "black/white"),
+				new Pet (2, "Milo", "mouse", "white"),
+				new Pet(3,"Otis","mouse", "black"),
+                new Pet(4,"Bella","cat", "orange"),
+                new Pet(5,"Derik","parrot", "green"),
+                new Pet(6,"Maggie","dog", "fawnequin"),
+                new Pet(7,"Ranger","dog", "black"),
+                new Pet(8,"Danny","ferret", "cream"),
+                new Pet(10,"Rosko","dog", "black/white")
 		};
 		
 		int id = (int)(10+Math.random()*20.0);
@@ -55,13 +55,13 @@ public class PetApp {
         System.out.println("Smallest pet ID: " + st.min());
         System.out.println("Smallest pet ID greater or equal to 17: " + st.ceiling(18));
         System.out.println("Number of pets: " + st.size());
-        st.put(30, new Pet(3, "Waldi", "dog"));
+        st.put(30, new Pet(3, "Waldi", "dog", "brown"));
         System.out.println("Pet IDs smaller 25: " + st.size(10, 25));
         System.out.println("Pet with ID 30: " + st.get(30));
         System.out.println("Second smallest pet ID: " + st.select(1));
         System.out.println();
 
-        st.put(30, new Pet(3, "Strolch", "Dog"));
+        st.put(30, new Pet(3, "Strolch", "Dog", "golden"));
 
         System.out.println(st.get(30));
         System.out.println();
